a457e1c79918cb2b7357c560c76bccdd8f70fcb9
Christian Michelsen <chris@circlestorm.org>
2020-01-04 23:54:52 +0100

    Added useState and useEffect

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,37 +1,29 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Header, List } from "semantic-ui-react";
 import axios from "axios";
 import { IActivity } from "../models/activity";
 
-interface IState {
-  activities: IActivity[]
-}
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
 
-class App extends Component<{}, IState> {
-  readonly state: IState = {
-    activities: []
-  };
-
-  componentDidMount() {
-    axios.get<IActivity[]>("http://localhost:5000/api/activities").then(response => {
-      this.setState({
-        activities: response.data
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then(response => {
+        setActivities(response.data);
       });
-    });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <Header as="h2" icon="users" content="Reactivities" />
-        <List>
-          {this.state.activities.map((activitity) => (
-            <List.Item key={activitity.id}>{activitity.title}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
+        {activities.map(activitity => (
+          <List.Item key={activitity.id}>{activitity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
 
 export default App;

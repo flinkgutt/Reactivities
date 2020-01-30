1adaa45a430829bf8f6f2d820857c271012546ff
Christian Michelsen <chris@circlestorm.org>
2020-01-30 21:41:34 +0100

    End of section 11

---
+++ client-app/src/app/common/form/TextInput.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FieldRenderProps } from 'react-final-form'
+import { FormFieldProps, Form, Label } from 'semantic-ui-react'
+
+interface IProps extends FieldRenderProps<string, HTMLElement>, FormFieldProps { }
+
+export const TextInput: React.FC<IProps> = ({ input, width, type, placeholder, meta: { touched, error } }) => {
+    return (
+        <Form.Field error={touched && !!error} width={width}>
+            <input {...input} placeholder={placeholder} type={type}/>
+            {touched && error && (
+                <Label basic color="red">{error}</Label>
+            )}
+        </Form.Field>
+    )
+}
